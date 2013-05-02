368dd7c0c598f20fb44cc61844295aeb24c6d2b0
Tim <ineeduisabel@gmail.com>
2013-05-02 17:46:56 +0800

    come on

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vincent::Application.config.secret_token = 'aa1a61fb800bb066c5ae10c11488f877fa3aa29f27aa9084dcf028b0be58c458e4a6faadcd93329c81542aedc47206ba1f5dd41801336597749f832e497e343e'
